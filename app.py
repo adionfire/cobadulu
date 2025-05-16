b2c63950a1c19e2ba80f759bdbd2b75dc18f250c
adionfire <abdillahbangun@gmail.com>
2025-05-16 16:21:13 +0700

    Tes CI/CD: Update Teks

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hallo dari Flask + Docker!"
+	return "Hallo dari CI/CD Otomatis!"
 
 if __name__== "__main__":
 	app.run(host="0.0.0.0", port=5000)
